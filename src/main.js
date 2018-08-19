0f47dbfcc78be7494bdae9991cb09bbe9b436db8
chenxiangling3150 <291105840@qq.com>
2018-08-19 13:13:45 +0800

    提交frist

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
